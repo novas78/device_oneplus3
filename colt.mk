add3e91e391b5aac68e4cba54bef39f1b271b8bd
SuperDroidBond <superdroidbond@yahoo.com>
2018-04-25 16:34:33 +0530

    Let's Make Colt Happen

--- colt.mk
+++ colt.mk
@@ -21,9 +21,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/oneplus3/device.mk)
 
 # Inherit some common gzosp stuff.
-$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
+$(call inherit-product, vendor/colt/config/common_full_phone.mk)
 
-PRODUCT_NAME := xtended_oneplus3
+PRODUCT_NAME := colt_oneplus3
 PRODUCT_DEVICE := oneplus3
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
@@ -31,3 +31,5 @@ PRODUCT_BRAND := OnePlus
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 TARGET_VENDOR := oneplus
+
+RELEASE_TYPE := COLT_OFFICIAL
