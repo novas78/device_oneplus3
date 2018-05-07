336ed3f260e15264b065e0720d30dd898d0acfaf
SuperDroid-BOND <superdroidbond@yahoo.com>
2018-05-07 10:36:00 +0530

    Delete Pocketmode & lineagehw. We don't need it.

--- device.mk
+++ device.mk
@@ -493,6 +493,3 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
-
-# Inherit from oppo-common
-$(call inherit-product, device/oppo/common/common.mk)
